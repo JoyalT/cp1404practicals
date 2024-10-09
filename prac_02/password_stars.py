8f07917cae64b7c1fa17f9f7284055ffdbb8576c
joyal <joyal.thomas@my.jcu.edu.au>
2024-10-10 02:12:37 +1000

    Add password masking functionality using asterisks

---
+++ prac_02/password_stars.py
@@ -0,0 +1,15 @@
+def main():
+    user_password = get_input()
+    masked_password = show_asterisks(user_password)
+    print(masked_password)
+
+
+def show_asterisks(user_password):
+    return '*' * len(user_password)
+
+def get_input():
+    user_password = input("Enter your password: ")
+    return user_password
+
+
+main()
